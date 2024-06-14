d678fe531f2eea081229cc67a5b0d9b80c4e3c0f
BAEK MIJIN <128350820+mijinB@users.noreply.github.com>
2024-06-14 02:20:08 +0000

    상속 + super

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-#Python은 public / private를 지원하지 않고, type도 지정해주지 않는다.
-#class 안에 있는 모든 메서드는 self를 가장 첫 번째 파라미터로 한다.
-class Player:
-  def __init__(self, name, xp):
+class Human:
+  def __init__(self, name):
     self.name = name
-    self.xp = xp
+
   def say_hello(self):
     print(f"hello my name is {self.name}")
 
-nico = Player('nico', 1000)
-nico.say_hello()
+
+class Player(Human):
+  def __init__(self, name, xp):
+    super().__init__(name)
+    self.xp = xp
+
+class Fan(Human):
+  def __init__(self, name, fav_team):
+    super().__init__(name)
+    self.fav_team = fav_team
+
+
+nico_player = Player("nico", 10)
+nico_player.say_hello()
+nico_fan = Fan("nico_fan", "blue")
+nico_fan.say_hello()
